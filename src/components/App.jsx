9ab31ccc162e81bd5094ab7cda7a888f7467c00b
Elegantniy <113132244+Elegantniy@users.noreply.github.com>
2023-05-25 19:57:39 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { ContactForm } from './ContactForm/ContactForm';
+import { useSelector } from 'react-redux';
+import { getContacts } from 'redux/selectors';
+
+export const App = () => {
+  const contacts = useSelector(getContacts);
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 30,
+        color: '#010101',
+      }}
+    >
+      <h2 className="formTitle">PhoneBook</h2>
+      <ContactForm />
+      <h2 className="contactListTitle">Contacts</h2>
+      {contacts.length > 0 ? (
+        <>
+          <Filter />
+          <ContactList  />
+        </>
+      ) : (
+        <p>Your have no contacts in your phone book</p>
+      )}
+    </div>
+  );
+};
